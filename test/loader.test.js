a51df8a7a0ae44900b9cac9c598583859b01ec4d
Andrew Branch <andrew@wheream.io>
2017-12-20 12:02:15 -0800

    Add test inspecting JSON output

--- test/loader.test.js
+++ test/loader.test.js
@@ -2,10 +2,12 @@ const path = require('path');
 const tsconfigPath = path.resolve(__dirname, 'tsconfig.json');
 const compiler = require('./compiler');
 
-test('anything', done => {
+test('writes a module exporting TypeDoc JSON', done => {
   compiler('test-file.ts').then(stats => {
     const output = stats.toJson().modules[0].source;
     expect(output).toMatch(/^export default /);
+    const json = JSON.parse(output.replace(/^export default /, ''));
+    expect(json.children[0].name).toBe('Animal');
     done();
   }).catch(done);
 });
